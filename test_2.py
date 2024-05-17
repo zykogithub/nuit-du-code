5fa2094439564140b03bf75df6eb79c5606e811f
zykogithub <modaherry@gmail.com>
2024-05-17 22:03:27 +0200

    version sans choix d'implémentation

--- test_2.py
+++ test_2.py
@@ -1,14 +1,6 @@
 chaine = "1, 2, 3, 5, 9, 12, -6, 3, 2, 101, 0, 5"
 def lire_csv(input_csv : str) -> list[list] :
     retour = [[],[]]
-    for element in input_csv :
-        if element==',' or element==' '  :
-            continue
-        else :
-            conversion = int(element)
-            if len(retour[0])<0:
-                retour[0].append(conversion)
-            else:
-                retour[1].append(conversion)
+    
     return retour
 lire_csv(chaine)
