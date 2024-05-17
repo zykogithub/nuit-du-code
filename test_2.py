3982532ac8a33d925c980fdb41d3dc54214e2460
zykogithub <modaherry@gmail.com>
2024-05-17 22:33:48 +0200

    correctif testes

--- test_2.py
+++ test_2.py
@@ -1,9 +1,12 @@
+# question 1
 chaine = "1, 2, 3, 5, 9, 12, -6, 3, 2, 101, 0, 5"
 def lire_csv(input_csv : str) -> list[list] :
     retour = [[],[]]
     
     return retour
 lire_csv(chaine)
+
+# question 2
 listeDoublon = [5, 1, 1, 2, 5, 6, 3, 4, 4, 4, 2]
 def tri_unique(liste : list) -> list :
     rendreUnique : set = set(liste)
@@ -12,7 +15,4 @@ def tri_unique(liste : list) -> list :
     return nouvelleListe
 listeDoublon=tri_unique(listeDoublon)
 print(listeDoublon)
-print(tri_unique([]))
-print(tri_unique([1]))
-tri_unique([-5, -1, 1, 2, 5, -6, 3, -4, 4, 4, 2])
         
