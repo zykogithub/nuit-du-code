a4ecf54e02626a93364bb136e40fbd4f1657a5b8
zykogithub <modaherry@gmail.com>
2024-05-29 09:06:54 +0200

    apprenstissage

--- apprenstissage_pyxel.py
+++ apprenstissage_pyxel.py
@@ -1,5 +1,12 @@
 import pyxel as py
 
-
+class App :
+    def __init__(self) -> None:
+        py.init(500,500,title="apprentissage",quit_key=py.KEY_BACKSPACE)
+        py.run(self.update,self.draw)
+    def update():
+        pass
+    def draw():
+        pass
 
 
